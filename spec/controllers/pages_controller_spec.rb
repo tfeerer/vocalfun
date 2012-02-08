45671a6a7249bd91b2ae384d7a57970466dd1d5c
tim <tfeerer@comcast.net>
2012-02-08 08:54:36 -0800

    Updated Gemfile

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
+ 	describe "GET 'home'" do
+    		it "should be successful" do
+      			get 'home'
+      			response.should be_success
+    		end
+ 	end
 end
